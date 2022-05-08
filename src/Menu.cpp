4246bd3c97925c21966113de643871cb66c97a11
PC_volt <PC_volt@DESKTOP-I20R60L>
2022-05-08 12:04:22 +0200

    can add menu elements now

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -10,16 +10,19 @@ void GetMenuPointer()
 
 }
 
+void AddMenuElement(MeltyLib::MenuSet *trainingMenuSet, const char *label, const char *name)
+{
+    MeltyLib::MenuElement *customElement = new MeltyLib::MenuElement(MeltyLib::NORMAL_ELEMENT, label, name);
+
+    *trainingMenuSet->pElementsListEnd = customElement;
+    trainingMenuSet->pElementsListEnd = trainingMenuSet->pElementsListEnd + 1; //increments the size of the list. pElementsListEnd doesn't need to be incremented for this to take effect.
+}
+
 void EditTrainingMenuElementsOrder()
 {
     MeltyLib::Menu *&trainingMenu = *reinterpret_cast<MeltyLib::Menu **>(MeltyLib::PTR_TRAININGMENU);
     MeltyLib::MenuSet *trainingMenuSet = *trainingMenu->ppMenuSet;
 
-
-    // At creation, the pElementsList points the end of the list (= pElementsListEnd), which is zero'd
-    // We could completely reorder the pElementsList
-    MeltyLib::MenuElement **beginning = trainingMenuSet->pElementsList - 20; //0x50 bytes = 80 bytes = 20 int, pre-existing MenuElement array
-
-    *trainingMenuSet->pElementsList = *(trainingMenuSet->pElementsList - 1); //copies the last element
-    trainingMenuSet->pElementsList = trainingMenuSet->pElementsList + 1; //increments the size of the list. pElementsListEnd doesn't need to be incremented for this to take effect.
+    AddMenuElement(trainingMenuSet, "CUSTOM 1", "CUSTOM_1");
+    AddMenuElement(trainingMenuSet, "CUSTOM 2", "CUSTOM_2");
 }
